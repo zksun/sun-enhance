ab9debb52f6d9962d60e7305405963acfdc3161c
缉熙 <zhikun.szk@alibaba-inc.com>
2016-05-12 20:04:08 +0800

    misc update

--- enhance-common/src/main/java/com/sun/enhance/asm/Clazz.java
+++ enhance-common/src/main/java/com/sun/enhance/asm/Clazz.java
@@ -1,8 +1,28 @@
 package com.sun.enhance.asm;
 
+import java.io.Serializable;
+
 /**
  * Created by zksun on 5/11/16.
  */
-public final class Clazz  {
+public final class Clazz implements Serializable {
 
+    private Feeld[] fields;
+    private Mezhod[] methods;
+
+    public Feeld[] getFields() {
+        return fields;
+    }
+
+    public void setFields(Feeld[] fields) {
+        this.fields = fields;
+    }
+
+    public Mezhod[] getMethods() {
+        return methods;
+    }
+
+    public void setMethods(Mezhod[] methods) {
+        this.methods = methods;
+    }
 }
---
+++ enhance-common/src/main/java/com/sun/enhance/asm/Feeld.java
@@ -0,0 +1,10 @@
+package com.sun.enhance.asm;
+
+import java.io.Serializable;
+
+/**
+ * Created by zksun on 5/12/16.
+ */
+public class Feeld implements Serializable {
+
+}
