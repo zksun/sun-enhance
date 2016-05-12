d7ed03093ae174e77f2975805c31242122e6aa41
缉熙 <zhikun.szk@alibaba-inc.com>
2016-05-12 14:27:45 +0800

    add junit logback pom dependency

--- enhance-common/src/main/java/com/sun/enhance/asm/Mezhod.java
+++ enhance-common/src/main/java/com/sun/enhance/asm/Mezhod.java
@@ -10,6 +10,46 @@ public class Mezhod {
     private Class<?>[] parameterTypes;
     private Class<?>[] exceptionTypes;
 
-    private int                 modifiers;
+    private int modifiers;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Class<?> getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(Class<?> returnType) {
+        this.returnType = returnType;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    public Class<?>[] getExceptionTypes() {
+        return exceptionTypes;
+    }
+
+    public void setExceptionTypes(Class<?>[] exceptionTypes) {
+        this.exceptionTypes = exceptionTypes;
+    }
+
+    public int getModifiers() {
+        return modifiers;
+    }
+
+    public void setModifiers(int modifiers) {
+        this.modifiers = modifiers;
+    }
 }
