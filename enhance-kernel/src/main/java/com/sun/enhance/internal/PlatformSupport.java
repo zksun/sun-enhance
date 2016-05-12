008d6edf7fb8f9d3f8e3834657c05f8a27f210cb
缉熙 <zhikun.szk@alibaba-inc.com>
2016-05-12 14:50:42 +0800

    add PlatformSupport class which has java.misc.unsafe support etc.

--- enhance-kernel/src/main/java/com/sun/enhance/internal/PlatformSupport.java
+++ enhance-kernel/src/main/java/com/sun/enhance/internal/PlatformSupport.java
@@ -1,8 +1,14 @@
 package com.sun.enhance.internal;
 
+import com.sun.enhance.logging.Logger;
+import com.sun.enhance.logging.LoggerFactory;
+
 /**
  * Created by zksun on 5/12/16.
  */
 public class PlatformSupport {
+
+    private final static Logger logger = LoggerFactory.getLogger(PlatformSupport.class);
+
 
 }
